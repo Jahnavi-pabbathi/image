64297c755f6c0a6560e40b2ede4dc9ab8ccdec64
unknown <geethanjaligandu2001@gmail.com>
2022-08-21 12:28:50 +0530

    changes in controller test cases

--- src/test/java/com/tgt/upcurve/ImageAPI/controller/ImageControllerTest.java
+++ src/test/java/com/tgt/upcurve/ImageAPI/controller/ImageControllerTest.java
@@ -16,47 +16,26 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 public class ImageControllerTest {
+
     @Autowired
     MockMvc mockMvc;
 
-    private static final String URI_GET_IMAGE_BY_IMAGE_ID = "/get_image/{id}";
+    private static final String URI_GET_IMAGE_BY_IMAGE_ID = "/image_api/v1/get_image/{id}";
     private static final String ORDER_JSON_FILE_PATH = "/imageData.json";
-    private static final String URI_GET_ORDER_ID_CUSTOMER_ID = "/generate_image/order_id/{order_id}/customer_id/{customer_id}";
+    private static final String URI_GET_ORDER_ID_CUSTOMER_ID = "/image_api/v1/generate_image/order_id/{order_id}/customer_id/{customer_id}";
     private static final String URI_SAVE = "/image_api/v1";
 
     @Test
-    public void testGetImageByImageId() throws Exception {
-        String imageString = JsonUtility.getResourceAsString(ORDER_JSON_FILE_PATH);
-        MvcResult responseSave = mockMvc.perform(post(URI_SAVE)
-                        .content(imageString)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().is2xxSuccessful())
-                .andReturn();
-        String savedResponse = responseSave.getResponse().getContentAsString();
-        MvcResult responseFetch = mockMvc.perform(get(URI_GET_IMAGE_BY_IMAGE_ID, 3)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().is2xxSuccessful())
-                .andReturn();
-        String fetchedResponse = responseFetch.getResponse().getContentAsString();
-        ImageEntity savedImageEntity = JsonUtility.readValue(savedResponse, ImageEntity.class);
-        ImageEntity fetchedImageEntity = JsonUtility.readValue(fetchedResponse, ImageEntity.class);
-        Assertions.assertEquals(savedImageEntity.getImageCode(), fetchedImageEntity.getImageCode());
-        Assertions.assertEquals(savedImageEntity.getId(), fetchedImageEntity.getId());
-    }
-
-    @Test
-    public void testGenerateImage() throws Exception {
-        String imageString = JsonUtility.getResourceAsString(ORDER_JSON_FILE_PATH);
-        MvcResult responseFetch = mockMvc.perform(get(URI_GET_ORDER_ID_CUSTOMER_ID, 1, 1)
+    public void test_GenerateImage() throws Exception {
+        MvcResult responseFetch = mockMvc.perform(get(URI_GET_ORDER_ID_CUSTOMER_ID, 1,1)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().is2xxSuccessful())
                 .andReturn();
@@ -65,5 +44,16 @@ public class ImageControllerTest {
         Assertions.assertEquals(fetchedImage.getId(), fetchedImage.getId());
         Assertions.assertNotNull(fetchedImage.getImageCode());
     }
-}
 
+    @Test
+    public void test_Generated_GetImage() throws Exception {
+        MvcResult responseFetch = mockMvc.perform(get(URI_GET_IMAGE_BY_IMAGE_ID, 1)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+        String fetchedResponse = responseFetch.getResponse().getContentAsString();
+        ImageEntity fetchedImageEntity = JsonUtility.readValue(fetchedResponse, ImageEntity.class);
+        Assertions.assertNotNull(fetchedImageEntity.getImageCode());
+        Assertions.assertEquals(1, fetchedImageEntity.getId());
+    }
+}
