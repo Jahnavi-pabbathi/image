c558dc00a35a8624b5f4ddd42f75ef425568cc55
Shubhani25 <shubhanimulay25@gmail.com>
2022-09-06 20:18:14 +0530

    Added json alias to ImageEntity

--- src/main/java/com/tgt/upcurve/ImageAPI/entity/ImageEntity.java
+++ src/main/java/com/tgt/upcurve/ImageAPI/entity/ImageEntity.java
@@ -1,8 +1,12 @@
 package com.tgt.upcurve.ImageAPI.entity;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -16,12 +20,19 @@ public class ImageEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
+    @JsonAlias("id")
     private Long id;
 
-    @Column(name = "image_code")
+    @Lob
+    @Type(type = "org.hibernate.type.BinaryType")
+    @Column(name="image_code",length=10000)
+    @JsonProperty("image_code")
+    @JsonAlias("imageCode")
     private byte[] imageCode;
 
     @Column(name = "created_at")
+    @JsonProperty("created_at")
+    @JsonAlias("createdAt")
     private LocalDateTime createdAt;
 
     @PrePersist
